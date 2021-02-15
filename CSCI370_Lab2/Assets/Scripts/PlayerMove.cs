e890d4ee9993db2f325d2786327dc9e0ff030589
Mitchell <mlrotenberry99@yahoo.com>
2021-02-15 00:38:12 -0600

    got snail sprite and camera moving

---
+++ CSCI370_Lab2/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    Rigidbody2D body;
+
+    private float horz, vert;
+    private float moveLimit = 0.7f;
+
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horz = Input.GetAxisRaw("Horizontal");
+        vert = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (horz != 0 && vert != 0)
+        {
+            horz *= moveLimit;
+            vert *= moveLimit;
+        }
+        body.velocity = new Vector2(horz * speed, vert * speed);
+    }
+}
