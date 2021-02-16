9bb423ca5b54ad8d0b79105bb741e4ac4ff10f36
Mitchell <mlrotenberry99@yahoo.com>
2021-02-15 20:08:24 -0600

    Created a second scene and got doors to travel between them

--- CSCI370_Lab2/Assets/Scripts/Doors.cs
+++ CSCI370_Lab2/Assets/Scripts/Doors.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Doors : MonoBehaviour
 {
+
+    public string toTravel;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,29 @@ public class Doors : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(LoadYourAsyncScene(toTravel));
+        }
+    }
+
+    IEnumerator LoadYourAsyncScene(string scene)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 }
